a57fc08960012cadfccc8faf241e872375198aef
Yael Cabrales <201912048@fit.edu.ph>
2023-02-28 08:09:32 +0800

    Add API in login

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
 import { Text, View, Image, TouchableOpacity, SafeAreaView, ScrollView, StatusBar, TextInput } from "react-native";
 
 export function Login({navigation}) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const emailHandler = (email) => {
+        setEmail(email);
+    }
+    const passwordHandler = (password) => {
+        setPassword(password);
+    }
+
+    //URL
+    const baseUrl = "http://192.168.1.59:8000";
+
+    const Login = async (event) => {
+        if (!email.trim()) {
+          alert("Required email.");
+          return;
+        }
+        if (!password.trim()) {
+            alert("Required password.");
+            return;
+        }
+        setIsLoading(true);
+        try {
+          const response = await axios.post(`${baseUrl}/api/v1/login`, {
+            email: email,
+            password: password,
+         });
+          if (response.status === 200) {
+            alert(`You are Login!`);
+            navigation.navigate('MainScreen')
+            setIsLoading(false);
+          } else {
+            throw new Error("An error has occurred");
+          }
+        } catch (error) {
+          alert(error);
+          setIsLoading(false);
+        }
+    };
+
         return (
         <View style={{flex: 1, paddingTop: Platform.OS === 'android' ? '10%' : 0}}>
             <View className="flex-1 bg-[#e7f4ff] items-center justify-center">
@@ -30,6 +73,8 @@ export function Login({navigation}) {
                         paddingVertical: 10,
                         marginTop: 10
                     }}
+                    onChangeText ={emailHandler}
+
                 />
                 <TextInput 
                     placeholder = 'Password'
@@ -40,6 +85,8 @@ export function Login({navigation}) {
                         paddingVertical: 10,
                         marginTop: 10
                     }}
+                    onChangeText ={passwordHandler}
+
                 />
                 <Text style = {{
                     color: '#0477A7',
@@ -48,7 +95,7 @@ export function Login({navigation}) {
                     marginTop: 10
                 }}>Forgot Password?</Text>
                 </View>
-                <TouchableOpacity onPress={() => navigation.navigate('MainScreen')} className="flex-2 items-center">
+                <TouchableOpacity onPress={Login} className="flex-2 items-center">
                     <Text className="font-bold text-base text-white bg-sky-800 rounded-xl text-center px-8 py-2.5 mb-10">Log in</Text>
                 </TouchableOpacity>
             </View>
