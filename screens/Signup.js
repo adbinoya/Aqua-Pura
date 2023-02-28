2ccb57cc1f67e5357df64eee4fcbf29a68863579
Yael Cabrales <201912048@fit.edu.ph>
2023-02-28 08:08:59 +0800

    Add API in signup

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,8 +1,69 @@
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios'
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View, Image, TouchableOpacity, ScrollView, TextInput } from "react-native";
 
 export function Signup({navigation}) {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const firstNameHandler = (fname) => {
+        setFirstName(fname);
+    }
+    const lastNameHandler = (lname) => {
+        setLastName(lname);
+    }
+    const phoneNumberHandler = (integer) => {
+        setPhoneNumber(integer);
+    }
+    const emailHandler = (email) => {
+        setEmail(email);
+    }
+    const passwordHandler = (password) => {
+        setPassword(password);
+    }
+
+    //URL
+    const baseUrl = "http://192.168.1.59:8000";
+
+    const Signup = async (event) => {
+        // if (!firstName.trim() || !email.trim() || !firstName.trim() || !email.trim()) {
+        //   alert("Name or Email is invalid");
+        //   return;
+        // }
+        setIsLoading(true);
+        try {
+          const response = await axios.post(`${baseUrl}/api/v1/register`, {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            contact: phoneNumber,
+            password: password,
+         });
+          if (response.status === 200) {
+            alert(`You are registered!`);
+            navigation.navigate('Login')
+            setFirstName('');
+            setLastName('');
+            setPhoneNumber('');
+            setEmail('');
+            setPassword('');
+            setIsLoading(false);
+          } else {
+            throw new Error("An error has occurred");
+          }
+        } catch (error) {
+          alert(error);
+          setIsLoading(false);
+        }
+    };
+    
     return (
         <View style={{flex: 1, paddingTop: Platform.OS === 'android' ? '10%' : 0}}>
               <ScrollView className="bg-[#e7f4ff] pt-[30%]">
@@ -25,6 +86,7 @@ export function Signup({navigation}) {
                                 paddingVertical: 10,
                                 marginTop: 10
                             }}
+                            onChangeText ={firstNameHandler}
                         />
                         <TextInput 
                             placeholder = 'Last Name'
@@ -35,6 +97,7 @@ export function Signup({navigation}) {
                                 paddingVertical: 10,
                                 marginTop: 10
                             }}
+                            onChangeText ={lastNameHandler}
                         />
                         <TextInput 
                             placeholder = 'Phone Number'
@@ -46,6 +109,7 @@ export function Signup({navigation}) {
                                 paddingVertical: 10,
                                 marginTop: 10
                             }}
+                            onChangeText ={phoneNumberHandler}
                         />
                         <TextInput 
                             placeholder = 'Email'
@@ -56,6 +120,7 @@ export function Signup({navigation}) {
                                 paddingVertical: 10,
                                 marginTop: 10
                             }}
+                            onChangeText ={emailHandler}
                         />
                         <TextInput 
                             placeholder = 'Password'
@@ -67,6 +132,7 @@ export function Signup({navigation}) {
                                 paddingVertical: 10,
                                 marginTop: 10
                             }}
+                            onChangeText ={passwordHandler}
                         />
                         <TextInput 
                             placeholder = 'Confirm Password'
@@ -79,9 +145,9 @@ export function Signup({navigation}) {
                                 marginTop: 10
                             }}
                         />
-                        </View>
+                        </View> 
                             <TouchableOpacity 
-                                onPress={() => navigation.navigate('Authentication')} 
+                                onPress={Signup}
                                 className="flex-2 items-center">
                                     <Text 
                                     className = " font-bold text-base text-white bg-sky-800 rounded-xl text-center px-8 py-2.5 mb-10"
